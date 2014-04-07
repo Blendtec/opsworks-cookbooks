d7e95f069713cde6a15e341ade8a42f3dedcbfd2
Orabpornpra <orabpornpra@blendtec.com>
2014-04-07 11:00:32 -0600

    Fixes T1241: Added new keys to distinguish apache and app, access and error

--- blendtec/recipes/td_agent.rb
+++ blendtec/recipes/td_agent.rb
@@ -19,7 +19,13 @@ node[:deploy].each do |app_name, deploy|
         :time_slice_format => (node['td_agent'][time_slice_format]),
         :time_slice_wait => (node['td_agent'][time_slice_wait]),
         :time_zone => (node['td_agent'][time_zone]),
-        :buffer_chunk_limit => (node['td_agent'][buffer_chunk_limit])
+        :buffer_chunk_limit => (node['td_agent'][buffer_chunk_limit]),
+        :apache_error_s3_object_key_format => (node['td_agent'][apache_error_s3_object_key_format]),
+        :apache_error_buffer_path => (node['td_agent'][apache_error_buffer_path]),
+        :access_s3_object_key_format => (node['td_agent'][access_s3_object_key_format]),
+        :access_error_s3_object_key_format => (node['td_agent'][access_error_s3_object_key_format]),
+        :access_buffer_path => (node['td_agent'][access_buffer_path]),
+        :access_error_buffer_path => (node['td_agent'][access_error_buffer_path])
     )
     only_if do
       File.directory?('/etc/td-agent/')
