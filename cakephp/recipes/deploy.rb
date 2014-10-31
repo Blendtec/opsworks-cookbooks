84fc8632123b1f4bb9a3ef8e8d9ff060791a67ca
Michael Clawson <mclawson@blendtec.com>
2014-10-31 08:49:32 -0600

    add SES config to cookbook
    Summary:
    WEB-11 #fixed
    add SES config to cookbook
    
    Test Plan: Once landed edit stack settings to use the develop branch of the custom cookbooks. Run Command: update custom cookbooks. Deploy. In the deploy log search for email.php and see that it used the template. Go and make sure emails still work on stage.
    
    Reviewers: #everyone, gwarner
    
    Reviewed By: #everyone, gwarner
    
    Subscribers: jslemboski
    
    Differential Revision: https://dev.blendtec.com/D1493

--- cakephp/recipes/deploy.rb
+++ cakephp/recipes/deploy.rb
@@ -57,6 +57,29 @@ node[:deploy].each do |app_name, deploy|
     end
   end
 
+  #generate email config file
+  template "#{deploy[:deploy_to]}/current/#{app_dir}Config/email.php" do
+    source 'email.php.erb'
+    mode 0440
+    group deploy[:group]
+
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
+    end
+
+    variables(
+        :key => (node['config']['email']['ses_key'] rescue nil),
+        :secret => (node['config']['email']['ses_secret'] rescue nil)
+        :region => (node['config']['email']['ses_region'] rescue nil)
+    )
+
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/current/#{app_dir}Config")
+    end
+  end
+
   #set permissions on cake console
   file "#{deploy[:deploy_to]}/current/#{app_dir}Console/cake" do
     if platform?('ubuntu')
