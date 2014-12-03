eb6b8d022bcac689db437d11e603462fcfe4fee9
Guy Warner <gwarner@blendtec.com>
2014-12-03 15:40:39 -0700

    Actually pull the right variable

--- cakephp/recipes/deploy.rb
+++ cakephp/recipes/deploy.rb
@@ -51,8 +51,8 @@ node[:deploy].each do |app_name, deploy|
         :cipher_seed => (node['config']['security']['cipher_seed'] rescue nil),
         :prefixes => (node['config']['prefixes'] rescue "'admin'"),
         :cache_engine => (node['config']['cache']['engine'] rescue nil),
-        :cache_session_server => (node['config']['cache']['server'] rescue nil),
-        :cache_data_server => (node['config']['cache']['server'] rescue nil),
+        :cache_session_server => (node['config']['cache']['session_server'] rescue nil),
+        :cache_data_server => (node['config']['cache']['data_server'] rescue nil),
         :hostname => (node[:opsworks][:instance][:hostname] rescue nil)
     )
 
