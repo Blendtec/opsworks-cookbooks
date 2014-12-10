94a4d58500f1f072369dfab93d5bc574d4446154
Guy Warner <gwarner@blendtec.com>
2014-12-10 10:57:34 -0700

    Timestamp added to core prefix
    Summary:
    Timestamp in core
    
    clean up file name
    
    Test Plan: On stage
    
    Reviewers: #everyone, mclawson
    
    Reviewed By: #everyone, mclawson
    
    Subscribers: mclawson
    
    Differential Revision: https://dev.blendtec.com/D1608

--- cakephp/recipes/deploy.rb
+++ cakephp/recipes/deploy.rb
@@ -7,6 +7,7 @@ include_recipe "composer::install"
 
 node[:deploy].each do |app_name, deploy|
   Chef::Log.info("CakePHP deploy #{app_name} to #{deploy[:deploy_to]}/current/#{app_name}")
+
   app_dir = node[:config][:app_dir] rescue "app/"
 
   #generate database config file
@@ -53,7 +54,8 @@ node[:deploy].each do |app_name, deploy|
         :cache_engine => (node['config']['cache']['engine'] rescue nil),
         :cache_session_server => (node['config']['cache']['session_server'] rescue nil),
         :cache_data_server => (node['config']['cache']['data_server'] rescue nil),
-        :hostname => (node[:opsworks][:instance][:hostname] rescue nil)
+        :hostname => (node[:opsworks][:instance][:hostname] rescue nil),
+        :timestamp => Time.now.to_i
     )
 
     only_if do
