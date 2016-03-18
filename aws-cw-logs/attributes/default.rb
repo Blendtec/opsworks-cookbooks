54006f7cb23881da15aa1cf6eba1636969713403
Michael Clawson <mclawson@blendtec.com>
2016-03-18 11:44:34 -0600

    AWS CloudWatch Logs apache and cakephp logs

---
+++ aws-cw-logs/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:cwlogs][:cakephp_error_log] = '/var/log/cakephp_error.log'
+default[:cwlogs][:apache_error_log] = '/var/log/apache2/error.log'
---
+++ aws-cw-logs/metadata.rb
@@ -0,0 +1,6 @@
+#metadata.rb
+
+name             'aws-cw-logs'
+version          '0.0.1'
+
+depends "python"
